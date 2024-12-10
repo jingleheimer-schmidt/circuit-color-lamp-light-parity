83b98754e5b9ea7cdf82b1360a57b9c48f67b121
asher_sky <46385945+jingleheimer-schmidt@users.noreply.github.com>
2024-12-09 21:33:23 -0800

    Update data.lua

--- data.lua
+++ data.lua
@@ -1,5 +1,5 @@
 
 for _, lamp in pairs(data.raw["lamp"]) do
     lamp.glow_color_intensity = lamp.light.intensity
-    lamp.glow_size = lamp.light.size
+    lamp.glow_size = lamp.light.size - 9
 end
